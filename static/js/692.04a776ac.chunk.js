2a4adfd6ba155706afce0fe4969493eebdd8584c
AlexandrRust <91406892+AlexandrRust@users.noreply.github.com>
2023-04-12 14:01:37 +0000

    Deploying to gh-pages from @ AlexandrRust/admin-panel@5a9ce350494b7e0f09d7e2db14c7bbccd2087c63 🚀

---
+++ static/js/692.04a776ac.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadmin_panel=self.webpackChunkadmin_panel||[]).push([[692],{7692:function(n,t,a){a.d(t,{gbs:function(){return u}});var e=a(9983);function u(n){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"}}]})(n)}}}]);
+//# sourceMappingURL=692.04a776ac.chunk.js.map
