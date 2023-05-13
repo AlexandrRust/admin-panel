30a80a9a38f37ac03de987f57f9c2f054f014ced
AlexandrRust <123_00@ukr.net>
2023-05-13 12:21:42 +0300

    change logo path

--- src/components/logo/LogoBox.jsx
+++ src/components/logo/LogoBox.jsx
@@ -1,14 +1,16 @@
 import { Logo } from './common/Logo.styled';
 import { LogoBoxStyle } from './common/LogoWrapper.styled';
+import logo from '../../image/logo192.png';
 
 const LogoBox = ({ isOpen, bigNavBar }) => {
   return (
     <LogoBoxStyle>
       <Logo to={'/'}>
         <img
-          src="http://shop.lockit.com.ua/dist/img/AdminLTELogo.png"
+          src={logo}
           alt="AdminLTE Logo"
           height={33}
+          style={{ borderRadius: '50%' }}
         />
         <h3
           style={
